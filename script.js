f3258c2d9861950e0dd3fefbee8ab5391c38bf9f
rhern <raulraulzinho@yahoo.com>
2026-05-24 23:35:16 -0400

    hashed date

--- script.js
+++ script.js
@@ -20,4 +20,19 @@ function getCentralTimeString() {
     return `${values.month}/${values.day}/${values.year} ${values.hour}:${values.minute}`;
 }
 
-paragraph.textContent = getCentralTimeString();
+async function sha256(input) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(input);
+
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    const hashHex = hashArray
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join("");
+
+    return hashHex;
+}
+
+paragraph.textContent = getCentralTimeString() + " " + (await sha256(getCentralTimeString()));
